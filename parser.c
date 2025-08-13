11ce70f392f4ec672594dee0238046416d78a291
lina <alina.eminova13@gmail.com>
2025-08-14 00:36:52 +0300

    parser.c modified

--- parser.c
+++ parser.c
@@ -1,13 +1,7 @@
-#include "lexer.h"
+#include "parser.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
-typedef struct {
-    Token *tokens;
-    int count;
-    int position;
-} TokenArray;
 
 TokenArray *tokenize(const char *input) {
     Lexer *lexer = lexer_init(input);
@@ -49,37 +43,6 @@ Token *next_token(TokenArray *array) {
     if (array->position >= array->count) return NULL;
     return &array->tokens[array->position++];
 }
-
-typedef enum {
-    NODE_COMMAND,
-    NODE_PIPE,
-    NODE_REDIRECTION,
-    NODE_AND,
-    NODE_OR,
-    NODE_SEQUENCE,
-    NODE_BACKGROUND,
-    NODE_SUBSHELL
-} NodeType;
-
-typedef struct ASTNode {
-    NodeType type;
-    struct ASTNode *left;
-    struct ASTNode *right;
-    char **args;
-    char *input_file;
-    char *output_file;
-    int append;
-} ASTNode;
-
-ASTNode *create_node(NodeType type, ASTNode *left, ASTNode *right);
-ASTNode *parse_command(TokenArray *array);
-ASTNode *parse_pipeline(TokenArray *array);
-ASTNode *parse_and_or(TokenArray *array);
-ASTNode *parse_sequence_background(TokenArray *array);
-ASTNode *parse_expression(TokenArray *array);
-void free_ast(ASTNode *node);
-void print_ast(ASTNode *node, int level);
-void test_parser(const char *input);
 
 ASTNode *create_node(NodeType type, ASTNode *left, ASTNode *right) {
     ASTNode *node = malloc(sizeof(ASTNode));
